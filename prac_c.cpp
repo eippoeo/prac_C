371bf20a09bfee07bef91d09d585c3ebe393d875
eippoeo <eippoeo@gmail.com>
2022-07-05 00:26:46 +0900

    prac_c

--- prac_c.cpp
+++ prac_c.cpp
@@ -3,8 +3,12 @@
 int main (){
 	int a = 1;
 	float b = 1.0;
-	printf("hello world");
-	printf("%d is interger",a);
-	printf("%f is double precision",b);
-	return 0;
-}
+	char c = 'a';
+	char s[4] = "abc";
+	printf("hello world\n");
+	printf("%d is interger\n",a);
+	printf("%f is single precision\n",b);
+	printf("%c\n",c);
+	printf("string is conticnus of \"char\" %s\n",s);
+	
+	}
